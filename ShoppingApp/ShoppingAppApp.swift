1fae3971f12387267d6bd363f5764156207091e0
kz <michal@babicz.dev>
2022-11-13 19:34:21 +0100

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by kz on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
